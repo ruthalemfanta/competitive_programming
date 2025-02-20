4aece6a16a640e8bd3a80a639a606758f17f2e56
Ruth Alemfanta <133218569+ruthalemfanta@users.noreply.github.com>
2025-02-20 10:11:12 +0300

    Time: 74 ms (18.90%) | Memory: 41.3 MB (64.89%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.py
+++ 2465-shifting-letters-ii/shifting-letters-ii.py
@@ -1,19 +1,23 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: list[list[int]]) -> str:
-        n = len(s)
-        shift_arr = [0] * (n + 1)
+        shift_arr = [0] * (len(s) + 1)
 
-        for shift in shifts:
-            start, end, direction = shift
-            shift_arr[start] += (1 if direction == 1 else -1)
-            if end + 1 < n:
-                shift_arr[end + 1] -= (1 if direction == 1 else -1)
+        for left, right, direction in shifts:  
+            shift_arr[left] += (1 if direction == 1 else -1) 
 
-        cur_shift = 0
-        shift_l = list(s)
-        for i in range(n):
-            cur_shift += shift_arr[i]
-            netShift = (cur_shift % 26 + 26) % 26
-            shift_l[i] = chr((ord(shift_l[i]) - ord('a') + netShift) % 26 + ord('a'))
+            if right + 1 < len(s):
+                shift_arr[right + 1] -= (1 if direction == 1 else -1)
+        prefix = []
+        acc = 0
 
-        return ''.join(shift_l)
+        for i in shift_arr:
+            acc += i
+            prefix.append(acc)
+
+        result = []
+        for i in range(len(s)):
+            result.append(chr((ord(s[i]) - ord('a') + (prefix[i] % 26 + 26) % 26) % 26 + ord('a')))
+
+        return ''.join(result) 
+
+
