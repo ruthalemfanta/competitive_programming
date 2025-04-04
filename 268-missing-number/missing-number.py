791dc349c707d7af7d9ba3485e74620214ab1702
Ruth Alemfanta <133218569+ruthalemfanta@users.noreply.github.com>
2025-04-04 22:51:41 +0300

    Time: 148 ms (16.66%) | Memory: 19.2 MB (6.58%) - LeetSync

---
+++ 268-missing-number/missing-number.py
@@ -0,0 +1,32 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        def merge(nums):
+            if len(nums) <= 1:
+                return nums
+            mid = len(nums) // 2
+            left = merge(nums[:mid])
+            right = merge(nums[mid:])
+
+            i = 0
+            j = 0
+            res = []
+            while i < len(left) and j < len(right):
+                if left[i] <= right[j]:
+                    res.append(left[i])
+                    i += 1
+                else:
+                    res.append(right[j])
+                    j += 1
+            
+            return res + left[i:] + right[j:]
+
+        numb = merge(nums)
+        print(numb)
+
+        for i in range(len(numb)):
+            if numb[i] != i :
+                return i
+
+        return numb[-1] + 1
+        
+
